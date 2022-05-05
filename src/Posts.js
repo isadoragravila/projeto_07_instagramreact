fb4dd70fac0f6933b33c98b4d40ac879db1ef35c
isadoraavila <isadoragravila@gmail.com>
2022-05-05 17:48:26 -0300

    Componentização - Stories e posts

---
+++ src/Posts.js
@@ -0,0 +1,98 @@
+export default function Posts () {
+    return (
+        <div class="posts">
+            <div class="post">
+                <div class="usuario">
+                    <div class="username">
+                        <div><img src="images/mitchell.jpg" /></div>
+                        <div><h3>mitchellpritchett</h3></div>
+                    </div>
+                    <div><ion-icon name="ellipsis-horizontal-sharp"></ion-icon></div>
+                </div>
+                <div class="imagem"><img src="images/post-mitchell.png" /></div>
+                <div class="curtir">
+                    <div class="reacoes">
+                        <div><ion-icon name="heart-outline"></ion-icon></div>
+                        <div><ion-icon name="chatbubble-outline"></ion-icon></div>
+                        <div><ion-icon name="paper-plane-outline"></ion-icon></div>
+                    </div>
+                    <div><ion-icon name="bookmark-outline"></ion-icon></div>
+                </div>
+                <div class="likes">
+                    <div><img src="images/cam.png" /></div>
+                    <div>Curtido por <strong>camtucker</strong> e <strong>outras 352 pessoas</strong></div>
+                </div>
+                <div class="margem-post"></div>
+            </div>
+            <div class="post">
+                <div class="usuario">
+                    <div class="username">
+                        <div><img src="images/gloria.png" /></div>
+                        <div><h3>gloriapritchett</h3></div>
+                    </div>
+                    <div><ion-icon name="ellipsis-horizontal-sharp"></ion-icon></div>
+                </div>
+                <div class="imagem"><img src="images/post-gloria.jpg" /></div>
+                <div class="curtir">
+                    <div class="reacoes">
+                        <div><ion-icon name="heart-outline"></ion-icon></div>
+                        <div><ion-icon name="chatbubble-outline"></ion-icon></div>
+                        <div><ion-icon name="paper-plane-outline"></ion-icon></div>
+                    </div>
+                    <div><ion-icon name="bookmark-outline"></ion-icon></div>
+                </div>
+                <div class="likes">
+                    <div><img src="images/phil.png" /></div>
+                    <div>Curtido por <strong>phildunphy</strong> e <strong>outras 387 pessoas</strong></div>
+                </div>
+                <div class="margem-post"></div>
+            </div>
+            <div class="post">
+                <div class="usuario">
+                    <div class="username">
+                        <div><img src="images/haley.jpg" /></div>
+                        <div><h3>haleydunphy</h3></div>
+                    </div>
+                    <div><ion-icon name="ellipsis-horizontal-sharp"></ion-icon></div>
+                </div>
+                <div class="imagem"><img src="images/post-haley.jpg" /></div>
+                <div class="curtir">
+                    <div class="reacoes">
+                        <div><ion-icon name="heart-outline"></ion-icon></div>
+                        <div><ion-icon name="chatbubble-outline"></ion-icon></div>
+                        <div><ion-icon name="paper-plane-outline"></ion-icon></div>
+                    </div>
+                    <div><ion-icon name="bookmark-outline"></ion-icon></div>
+                </div>
+                <div class="likes">
+                    <div><img src="images/alex.jpg" /></div>
+                    <div>Curtido por <strong>alexdunphy</strong> e <strong>outras 395 pessoas</strong></div>
+                </div>
+                <div class="margem-post"></div>
+            </div>
+            <div class="post">
+                <div class="usuario">
+                    <div class="username">
+                        <div><img src="images/phil.png" /></div>
+                        <div><h3>phildunphy</h3></div>
+                    </div>
+                    <div><ion-icon name="ellipsis-horizontal-sharp"></ion-icon></div>
+                </div>
+                <div class="imagem"><img src="images/post-phil.jpg" /></div>
+                <div class="curtir">
+                    <div class="reacoes">
+                        <div><ion-icon name="heart-outline"></ion-icon></div>
+                        <div><ion-icon name="chatbubble-outline"></ion-icon></div>
+                        <div><ion-icon name="paper-plane-outline"></ion-icon></div>
+                    </div>
+                    <div><ion-icon name="bookmark-outline"></ion-icon></div>
+                </div>
+                <div class="likes">
+                    <div><img src="images/luke.jpg" /></div>
+                    <div>Curtido por <strong>lukedunphy</strong> e <strong>outras 236 pessoas</strong></div>
+                </div>
+                <div class="margem-post"></div>
+            </div>
+        </div>
+    );
+}
---
+++ src/Stories.js
@@ -0,0 +1,52 @@
+export default function Stories () {
+    return (
+        <div class="stories">
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/alex.jpg" /></div>
+                <p>alexdunphy</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/gloria.png" /></div>
+                <p>gloriaprit...</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/phil.png" /></div>
+                <p>phildunphy</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/cam.png" /></div>
+                <p>camtucker</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/haley.jpg" /></div>
+                <p>haleydunphy</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/mitchell.jpg" /></div>
+                <p>mitchellpri...</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/manny.jpg" /></div>
+                <p>mannydel...</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/luke.jpg" /></div>
+                <p>lukedunphy</p>
+            </div>
+            <div class="story">
+                <img src="images/stories_background.jpg" />
+                <div class="imagem-story"><img src="images/usuario1.png" /></div>
+                <p>username</p>
+            </div>
+            <div class="seta-stories"><ion-icon name="chevron-forward-circle-sharp"></ion-icon></div>
+        </div>
+    );
+}
