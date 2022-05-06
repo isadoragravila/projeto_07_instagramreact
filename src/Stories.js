3c0bc7c1cf5026e8ac08b4569054256839d731a3
isadoraavila <isadoragravila@gmail.com>
2022-05-06 13:47:14 -0300

    Props - Stories e posts

--- src/Stories.js
+++ src/Stories.js
@@ -1,51 +1,29 @@
+function Story (props) {
+    return (
+        <div class="story">
+            <img src="images/stories_background.jpg" />
+            <div class="imagem-story"><img src={props.image} /></div>
+            <p>{props.name}</p>
+        </div>
+    );
+}
+
 export default function Stories () {
+    const storyUsers = [
+        {name: "alexdunphy" , image: "images/alex.jpg"},
+        {name: "gloriaprit..." , image: "images/gloria.png"},
+        {name: "phildunphy" , image: "images/phil.png"},
+        {name: "camtucker" , image: "images/cam.png"},
+        {name: "haleydunphy" , image: "images/haley.jpg"},
+        {name: "mitchellpri..." , image: "images/mitchell.jpg"},
+        {name: "mannydel..." , image: "images/manny.jpg"},
+        {name: "lukedunphy" , image: "images/luke.jpg"},
+        {name: "username" , image: "images/usuario1.png"},
+    ];
+    
     return (
         <div class="stories">
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/alex.jpg" /></div>
-                <p>alexdunphy</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/gloria.png" /></div>
-                <p>gloriaprit...</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/phil.png" /></div>
-                <p>phildunphy</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/cam.png" /></div>
-                <p>camtucker</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/haley.jpg" /></div>
-                <p>haleydunphy</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/mitchell.jpg" /></div>
-                <p>mitchellpri...</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/manny.jpg" /></div>
-                <p>mannydel...</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/luke.jpg" /></div>
-                <p>lukedunphy</p>
-            </div>
-            <div class="story">
-                <img src="images/stories_background.jpg" />
-                <div class="imagem-story"><img src="images/usuario1.png" /></div>
-                <p>username</p>
-            </div>
+            {storyUsers.map(user => <Story name={user.name} image={user.image} />)}
             <div class="seta-stories"><ion-icon name="chevron-forward-circle-sharp"></ion-icon></div>
         </div>
     );
